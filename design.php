97564a93c12f65ce6c5f5485107bd07fbf871c93
Andrew Mason <andrewmasonmedia@gmail.com>
2019-02-23 17:48:41 -0500

    Added bhs logo images to design page, updated copy

--- design.php
+++ design.php
@@ -1,6 +1,6 @@
 <?php
 
-$title = "Decentral | About Us";
+$title = "Mason Media | Design";
 
 $metaD = "Partner with Decentral to capitalize on the knowledge, experience, and network of Canada's leading blockchain company.";
 
@@ -15,7 +15,11 @@ include 'includes/header.php';
             <img class="img-full absolute z-0 left-0 up up-med lazy" data-src="https://images.unsplash.com/photo-1494253109108-2e30c049369b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80" alt="About Decentral">
             <div class="col-lg-9 d-flex flex-column justify-content-center align-items-start text-left text-light p-5">
                 <h1 class="h4 mt-3">Design</h1>
-                <h2 class="site-title knockout-rainbow font-weight-bold">Bold. Graphic. Asymmetrical.</h2>
+                <h2 class="site-title knockout-rainbow font-weight-bold">
+                    <span class="d-table">Bold.</span>
+                    <span class="d-table">Graphic.</span>
+                    <span class="d-table">Asymmetrical.</span>
+                </h2>
                 <a href="#sec-1"><i class="fa fa-angle-down p-0"></i></a>
             </div>
             <div class="offset-lg-3"></div>
@@ -24,15 +28,14 @@ include 'includes/header.php';
     
     <div class="container-fluid p-0 relative">
 
-        <div class="row d-flex p-5 min-500 text-secondary bg-white">
+        <div id="sec-1" class="row d-flex p-5 min-500 text-secondary bg-white">
             <div class="col-lg-6 d-flex flex-column justify-content-center align-items-start text-left">
                 <h2 class="h4 text-primary">Mason Media</h2>
                 <h3 class="section-title text-dark mb-0">Design for web + print.</h3>
-                <p class="p-big pt-3 m-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum excepturi tempore possimus illum amet sed nihil ea aspernatur eveniet rem dignissimos, sequi, numquam iste suscipit commodi minima, assumenda quidem ducimus!</p>
+                <p class="p-big pt-3 m-0">I believe in the power of design to inspire, shift mindsets, create positivity, and unleash joy. Every day, I work to improve, grow, take risks, and outdo what I did the previous day.</p>
             </div>
             <div class="col-lg-6 d-flex flex-column justify-content-center align-items-start text-left o-12">
-                <p class="p-big pt-3 m-0">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis quaerat sint totam distinctio iste rerum debitis, vitae porro consectetur molestiae, accusamus id optio. Voluptatum, odit! Tempore facilis, maiores dolores eius.</p>
-                <a href="https://jaxx.io"><p class="p-btn mt-3">Find out more &#8594;</p></a>
+                <p class="p-big pt-3 m-0">I use photoshop, canva, crello, wireframe.cc, mockflow and other tools to design for web and print media, websites and user interfaces, user experience design, social media content, logos, posters, album/track artwork for bands, and more. I experiment ceaselessly and strive for bold, creative, lateral thinking in all I do. Explore some of my work below.</p>
             </div>
         </div><!--end row-->  
         
@@ -48,14 +51,13 @@ include 'includes/header.php';
             <div class="row d-flex min-500 text-secondary bg-white">
                 <div class="offset-lg-2"></div>
                 <div class="col-lg-8 d-flex flex-column justify-content-center align-items-center text-center p-5">
-                    <h2 class="h4 text-primary">Business cards + posters</h2>
-                    <h3 class="section-title text-dark mb-0">Aimee Claxton.</h3>
-                    <p class="p-big pt-3 m-0">Vocalist, bandleader, and music instructor Aimee Claxton is talented composer and performer on the Toronto jazz music landscape who required a number of designs for her performing and teaching businesses. To right is the design for her performing needs.</p>
+                    <h2 class="h4 text-primary">Aimee Claxton</h2>
+                    <h3 class="section-title text-dark mb-0">Business cards + posters.</h3>
+                    <p class="p-big pt-3 m-0">Vocalist, bandleader, and music instructor Aimee Claxton is talented composer and performer on the Toronto jazz music landscape who required a number of designs for her performing and teaching businesses. Designs included business cards, poster + social media designs for her live concerts, as well handbills to market her teaching studio.</p>
                 </div>
                 <div class="offset-lg-2"></div>
             </div>
         </section>
-    
         
         <div class="row d-flex min-400 text-secondary bg-white">
             <div class="col-lg-6 min-500 d-flex flex-column justify-content-center align-items-start text-left p-0 o-12">
@@ -76,7 +78,7 @@ include 'includes/header.php';
             <div class="offset-lg-2"></div>
             <div class="col-lg-8 d-flex flex-column justify-content-center align-items-center text-center p-5">
                 <h2 class="h4 text-primary">Ed Mortenson</h2>
-                <h3 class="section-title text-dark mb-0">Business cards + posters.</h3>
+                <h3 class="section-title text-dark mb-0">Business cards.</h3>
                 <p class="p-big pt-3 m-0">Toronto drummer and music instructor Ed Mortenson required business card designs for his performing and teaching businesses, as well as for his services driving for uber. He requested a design that blended part edgy, rock personality, with the clean, trustworthiness of a professional service. I came up with a layout equal parts simple, unique, bold, and rock.</p>
             </div>
             <div class="offset-lg-2"></div>
@@ -113,18 +115,18 @@ include 'includes/header.php';
         </section>
         
         <section>
-            <div class="row d-flex bg-light px-4 bg-light">
-                <div class="col-lg-6 d-flex flex-column justify-content-center align-items-start text-left pt-4 o-12">
-                    <img class="w-100 ds lazy" data-src="/assets/img/wiwp-facebook-cover-mockup-forest.jpg" alt="About Decentral">
+            <div class="row d-flex bg-light bg-light">
+                <div class="col-lg-6 d-flex flex-column justify-content-center align-items-start text-left p-0 o-12">
+                    <img class="w-100 lazy" data-src="/assets/img/wiwp-facebook-cover-mockup-forest.jpg" alt="Janice Mason Steeves Workshops in Wild Places.">
                 </div>
-                <div class="col-lg-6 d-flex flex-column justify-content-center align-items-start text-left pt-4 o-12">
-                    <img class="w-100 ds lazy" data-src="/assets/img/wiwp-facebook-cover-mockup-beach.jpg" alt="About Decentral">
+                <div class="col-lg-6 d-flex flex-column justify-content-center align-items-start text-left p-0 o-12">
+                    <img class="w-100 lazy" data-src="/assets/img/wiwp-facebook-cover-mockup-beach.jpg" alt="Janice Mason Steeves Workshops in Wild Places.">
                 </div>
-                <div class="col-lg-6 d-flex flex-column justify-content-center align-items-start text-left pt-4 o-12">
-                    <img class="w-100 ds lazy" data-src="/assets/img/wiwp-facebook-cover-mockup-mountains.jpg" alt="About Decentral">
+                <div class="col-lg-6 d-flex flex-column justify-content-center align-items-start text-left p-0 o-12">
+                    <img class="w-100 lazy" data-src="/assets/img/wiwp-facebook-cover-mockup-mountains.jpg" alt="Janice Mason Steeves Workshops in Wild Places.">
                 </div>
-                <div class="col-lg-6 d-flex flex-column justify-content-center align-items-start text-left pt-4 o-12">
-                    <img class="w-100 ds lazy" data-src="/assets/img/wiwp-facebook-cover-mockup-ocean.jpg" alt="About Decentral">
+                <div class="col-lg-6 d-flex flex-column justify-content-center align-items-start text-left p-0 o-12">
+                    <img class="w-100 lazy" data-src="/assets/img/wiwp-facebook-cover-mockup-ocean.jpg" alt="Janice Mason Steeves Workshops in Wild Places.">
                 </div>
             </div>
         </section>
@@ -136,14 +138,28 @@ include 'includes/header.php';
                 <div class="offset-lg-2"></div>
                 <div class="col-lg-8 d-flex flex-column justify-content-center align-items-center text-center p-5">
                     <h2 class="h4 text-primary">Big Horn Sheep</h2>
-                    <h3 class="section-title text-dark mb-0">Posters + social media.</h3>
-                    <p class="p-big pt-3 m-0">Canadian power trio <a href="http://bighornsheepband.com" target="_blank"><span class="text-primary">Big Horn Sheep</span></a> are a Toronto based band with an original repertoire uniquely blending jazz, funk, EDM, and rock-pop. They play regularly throughout Toronto and required a complete design mockup including logo, website, concert posters, and social media content. The resulting design incorporates an atari-influenced 8-bit video game theme including bold neon colors, a playful take on a sheep's head for the logo, simple yet bold sans-serif fonts, and myriad variations on the theme for social media content. See more below.</p>
+                    <h3 class="section-title text-dark mb-0">Logo + posters + social media.</h3>
+                    <p class="p-big pt-3 m-0"><a href="http://bighornsheepband.com" target="_blank"><span class="text-primary">Big Horn Sheep</span></a> are a Toronto based instrumental trio with an original repertoire that uniquely blends jazz, funk, EDM, and rock-pop. They play regularly throughout Toronto and required a complete design mockup including logo, website, concert posters, album cover art, and social media content. The resulting design incorporates an atari-influenced, 8-bit video game theme including bold neon colors, a playful take on a sheep's head for the logo, simple yet bold sans-serif fonts, and myriad variations on the theme for social media content.</p>
                 </div>
                 <div class="offset-lg-2"></div>
             </div><!--end row-->
         </section>
         
         <!-- bhs images-->
+        
+        <section>
+            <div class="row d-flex min-400 text-secondary bg-white">
+                <div class="col-lg-4 min-500 d-flex flex-column justify-content-center align-items-start text-left p-0 o-12">
+                    <img class="img-full lazy" data-src="/assets/img/bhs-logo-blue.jpg" alt="Big Horn Sheep Facebook cover design">
+                </div>
+                <div class="col-lg-4 min-500 d-flex flex-column justify-content-center align-items-start text-left p-0 o-12">
+                    <img class="img-full lazy" data-src="/assets/img/bhs-logo-green.jpg" alt="Big Horn Sheep Facebook cover design">
+                </div>
+                <div class="col-lg-4 min-500 d-flex flex-column justify-content-center align-items-start text-left p-0 o-12">
+                    <img class="img-full lazy" data-src="/assets/img/bhs-logo-red.jpg" alt="Big Horn Sheep Facebook cover design">
+                </div>
+            </div>
+        </section>
         
         <section>
             <div class="row d-flex min-400 text-secondary bg-white">
@@ -179,7 +195,7 @@ include 'includes/header.php';
                 <div class="col-lg-8 d-flex flex-column justify-content-center align-items-center text-center p-5">
                     <h2 class="h4 text-primary">Jen Mason, PhD.</h2>
                     <h3 class="section-title text-dark mb-0">Flyers + posters + website.</h3>
-                    <p class="p-big pt-3 m-0">For <a href="http://jenmason.ca" target="_blank"><span class="text-primary">Jen Mason</span></a>, PhD's dissertation research project, she conducted a joint workshop on the role of mindfulness in promoting dialogue among people with diverse and varied perspectives. The goal was to facilitate and foster an improvement in the ability to communicate in multi-stakeholder situations, specifically with regard to topics including sustainability and the environment. Mrs. Mason required a design workup including a website, poster, and (below) a flyer for advertising.</p>
+                    <p class="p-big pt-3 m-0">For <a href="http://jenmason.ca" target="_blank"><span class="text-primary">Jen Mason</span></a> PhD's dissertation research project, she conducted a joint workshop on the role of mindfulness in promoting dialogue among people with diverse and varied perspectives. The goal was to facilitate and foster an improvement in the ability to communicate in multi-stakeholder situations, specifically with regard to topics including sustainability and the environment. Mrs. Mason required a design workup including a website, poster, and (below) a flyer for advertising.</p>
                 </div>
                 <div class="offset-lg-2"></div>
             </div><!--end row-->
